e73e8156def10d91db807978c7db0f45fb0d14cf
Djcharles26 <jlaraanaya@gmail.com>
2025-02-09 09:46:25 -0600

    Updated sensor types models & enums

--- lib/src/orchestrator/models/sensor_type.dart
+++ lib/src/orchestrator/models/sensor_type.dart
@@ -2,22 +2,26 @@ enum SensorType {
   /// A built-in wide-angle camera.
   ///
   /// The wide angle sensor is the default sensor for iOS
-  wideAngle,
+  wideAngle (1),
 
   /// A built-in camera with a shorter focal length than that of the wide-angle camera.
-  ultraWideAngle,
+  ultraWideAngle (0),
 
   /// A built-in camera device with a longer focal length than the wide-angle camera.
-  telephoto,
+  telephoto (2),
 
   /// A device that consists of two cameras, one Infrared and one YUV.
   ///
   /// iOS only
-  trueDepth,
+  trueDepth (3),
 
-  unknown;
+  unknown (4);
+
+  final int order;
 
   SensorType get defaultSensorType => SensorType.wideAngle;
+
+  const SensorType (this.order);
 }
 
 class SensorTypeDevice {
@@ -35,13 +39,35 @@ class SensorTypeDevice {
   /// An identifier that uniquely identifies the device.
   final String uid;
 
+  /// Available min zoom;
+  final num minZoom;
+
+  /// Available Optical (no distorsion) zoom
+  final num maxOpticalZoom;
+
+  /// Available Digital Zoom;
+  final num maxDigitalZoom;
+
   SensorTypeDevice({
     required this.sensorType,
     required this.name,
     required this.iso,
     required this.flashAvailable,
     required this.uid,
+    required this.minZoom,
+    required this.maxOpticalZoom,
+    required this.maxDigitalZoom
   });
+
+  @override
+  String toString() {
+    return "Sensor: $sensorType - $name\n"
+      "ISO: $iso\n"
+      "Flash Available: $flashAvailable\n"
+      "UID: $uid\n"
+      "Min Zoom: $minZoom\n"
+      "Max Opt Zoom: $maxOpticalZoom - Max Dig Zoom: $maxDigitalZoom";
+  }
 }
 
 // TODO: instead of storing SensorTypeDevice values,
@@ -84,13 +110,38 @@ class SensorDeviceData {
     ].where((element) => element != null).cast<SensorTypeDevice>().toList();
   }
 
+  List<SensorTypeDevice?> get backSensors => [
+    wideAngle,
+    ultraWideAngle,
+    telephoto,
+  ];
+
+  List<SensorTypeDevice?> get frontSensors => [
+    trueDepth
+  ];
+
   int get availableBackSensors => [
         wideAngle,
         ultraWideAngle,
         telephoto,
-      ].where((element) => element != null).length;
+      ].nonNulls.length;
 
   int get availableFrontSensors => [
         trueDepth,
-      ].where((element) => element != null).length;
+      ].nonNulls.length;
+
+  SensorTypeDevice? deviceFromType (SensorType type) {
+    switch (type) {
+      case SensorType.wideAngle:
+        return wideAngle;
+      case SensorType.ultraWideAngle:
+        return ultraWideAngle;
+      case SensorType.telephoto:
+        return telephoto;
+      case SensorType.trueDepth:
+        return trueDepth;
+      case SensorType.unknown:
+        return null;
+    }
+  }
 }
